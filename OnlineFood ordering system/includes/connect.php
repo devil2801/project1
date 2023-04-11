a9efd6f49bf38087e74b7260f1ddd9ea09993253
devil2801 <72138573+devil2801@users.noreply.github.com>
2023-04-11 12:08:35 +0530

    Add files via upload

---
+++ OnlineFood ordering system/includes/connect.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+$servername = "localhost";
+$server_user = "root";
+$server_pass = "";
+$dbname = "food";
+
+if(isset($name) || isset($role))
+{
+
+$name = $_SESSION['name'];
+$role = $_SESSION['role'];
+
+}
+
+$con = new mysqli($servername, $server_user, $server_pass, $dbname);
+?>
